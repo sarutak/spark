d2e58dcccbc0f0f74faa636ac6a61b39d74e0326
Kousuke Saruta <sarutak@amazon.co.jp>
2026-05-28 04:24:40 +0900

    Benchmark: OFF only (SF=5)

--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/TPCDSQueryBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/TPCDSQueryBenchmark.scala
@@ -111,10 +111,6 @@ object TPCDSQueryBenchmark extends SqlBasedBenchmark with Logging {
         spark.conf.set(SQLConf.MERGE_SUBPLANS_GROUPING_AGGREGATE_ENABLED.key, "false")
         spark.sql(queryString).noop()
       }
-      benchmark.addCase(s"$name$nameSuffix (grouping agg merge)") { _ =>
-        spark.conf.set(SQLConf.MERGE_SUBPLANS_GROUPING_AGGREGATE_ENABLED.key, "true")
-        spark.sql(queryString).noop()
-      }
       benchmark.run()
     }
   }
